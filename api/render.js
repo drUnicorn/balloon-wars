36f5006b530a1662cad786e8646a6b39ac22fc0e
m93a <m93a.cz@gmail.com>
2014-10-17 19:05:32 +0200

    the great cleanup
    odstraněny nepotřebné soubory, kód vyčištěn a okomentován

--- api/render.js
+++ api/render.js
@@ -1,193 +1,126 @@
+
+
+Physijs.scripts.worker = window.location+'/api/physijs_worker.js';
+Physijs.scripts.ammo = window.location+'/api/ammo.js';
+
+
+
+var initScene, render, createShape, NoiseGen,
+ renderer, scene, light, camera, shape;
+
+initScene = function() {
  
- Physijs.scripts.worker = '/map/api/physijs_worker.js';
- Physijs.scripts.ammo = '/map/api/ammo.js';
+ //TWEEN je pro plynulé animace
+ TWEEN.start();
  
- var initScene, render, createShape, NoiseGen,
-  renderer, scene, light, camera, shape;
+ //Nastav renderovací jádro, alpha znamená průhledné pozadí
+ renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+ renderer.setSize( window.innerWidth, window.innerHeight );
+ renderer.shadowMapEnabled = true;
+ renderer.shadowMapSoft = true;
+ document.getElementById( 'canvas' ).appendChild( renderer.domElement );
  
- initScene = function() {
-  TWEEN.start();
-  
-  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  renderer.shadowMapEnabled = true;
-  renderer.shadowMapSoft = true;
-  document.getElementById( 'canvas' ).appendChild( renderer.domElement );
-  
-  scene = new Physijs.Scene({ fixedTimeStep: 1 / 120 });
-  scene.setGravity(new THREE.Vector3( 0, 0, 0 ));
-  scene.addEventListener(
-   'update',
-   function() {
-    scene.simulate( undefined, 2 );
-   }
-  );
-  
-  
-  camera = new THREE.PerspectiveCamera(
-   60,
-   window.innerWidth / window.innerHeight,
-   1,
-   1000
-  );
-  scene.add( camera );
-  
-  // Light
-  light = new THREE.DirectionalLight( 0xFFFFFF );
-  light.position.set( 20, 40, -15 );
-  light.target.position.copy( scene.position );
-  light.castShadow = true;
-  light.shadowCameraLeft = -60;
-  light.shadowCameraTop = -60;
-  light.shadowCameraRight = 60;
-  light.shadowCameraBottom = 60;
-  light.shadowCameraNear = 20;
-  light.shadowCameraFar = 200;
-  light.shadowBias = -.0001
-  light.shadowMapWidth = light.shadowMapHeight = 2048;
-  light.shadowDarkness = .7;
-  scene.add( light );
-  
-  
-  
-  requestAnimationFrame( render );
-  scene.simulate();
-  
-  createShape();
- };
- 
- var cameraPosSet = false;
- 
- render = function() {
-  requestAnimationFrame( render );
-  renderer.render( scene, camera );
-  if(browser&&browser.getPosition()[0]){
-   if(!cameraPosSet){
-    browser.cameraCopyTo( camera );
-    camera.rotation.set(0,0,0);
-    cameraPosSet = true;
-   }else{
-    browser.cameraCopyTo( camera );
-   }
+ //Vytvoř scénu
+ scene = new Physijs.Scene({ fixedTimeStep: 1 / 120 });
+ scene.setGravity(new THREE.Vector3( 0, 0, 0 ));
+ scene.addEventListener(
+  'update',
+  function() {
+   scene.simulate( undefined, 2 );
   }
+ );
+ 
+ 
+ //Vytvoř kameru
+ camera = new THREE.PerspectiveCamera(
+  60,
+  window.innerWidth / window.innerHeight,
+  1,
+  1000
+ );
+ scene.add( camera );
+ 
+ 
+ //Osvětli scénu
+ light = new THREE.DirectionalLight( 0xFFFFFF );
+ light.position.set( 20, 40, -15 );
+ light.target.position.copy( scene.position );
+ light.castShadow = true;
+ light.shadowCameraLeft = -60;
+ light.shadowCameraTop = -60;
+ light.shadowCameraRight = 60;
+ light.shadowCameraBottom = 60;
+ light.shadowCameraNear = 20;
+ light.shadowCameraFar = 200;
+ light.shadowBias = -.0001
+ light.shadowMapWidth = light.shadowMapHeight = 2048;
+ light.shadowDarkness = .7;
+ scene.add( light );
+ 
+ 
+ //Spusť renderování
+ requestAnimationFrame( render );
+ scene.simulate();
+ 
+ //Vytvoř krychli
+ createShape();
+};
+
+
+//Synchronizuj kameru mapy a THREE.js
+render = function() {
+ requestAnimationFrame( render );
+ renderer.render( scene, camera );
+ if(browser&&browser.getPosition()[0]){
+  browser.cameraCopyTo( camera );
+ }
+ 
+};
+
+
+//Geometrie a materiál krychle
+var box_geometry = new THREE.BoxGeometry( 3, 3, 3 );
+var material = new THREE.MeshLambertMaterial({ opacity: 0, transparent: true });
+
+
+ createShape = function() {
+  
+  //Vytvoř krychli z geometrie a materiálu
+  shape = new Physijs.BoxMesh(
+     box_geometry,
+     material
+  );
   
   
-  //camera.lookAt( shape.position );
+  //Náhodně vyber barvu
+  shape.material.color.setRGB( Math.random() * 100 / 100, Math.random() * 100 / 100, Math.random() * 100 / 100 );
+  
+  shape.castShadow = true;    //Krychle vytváří stín
+  shape.receiveShadow = true; //A může na ni dopadat stín
+  
+  
+  //Nastav pozici
+  shape.position.set(
+   -458133.9720997899, //UTM šířka
+   261,                //nadmořská výška
+   5548777.776496544   //UTM délka
+  );
+  
+  
+  //Nastav náhodnou rotaci
+  shape.rotation.set(
+   Math.random() * Math.PI,
+   Math.random() * Math.PI,
+   Math.random() * Math.PI
+  );
+  
+  
+  scene.add( shape ); //Vlož krychli do scény
+  
+  
+  //Animace do neprůhlednosti
+  new TWEEN.Tween(shape.material).to({opacity: 1}, 500).start();
+  
  };
- 
- var box_geometry = new THREE.BoxGeometry( 3, 3, 3 );
- 
-  
-  createShape = function() {
-   var material = new THREE.MeshLambertMaterial({ opacity: 0, transparent: true });
-   
-   
-     shape = new Physijs.BoxMesh(
-      box_geometry,
-      material
-     );
-    
-   shape.material.color.setRGB( Math.random() * 100 / 100, Math.random() * 100 / 100, Math.random() * 100 / 100 );
-   shape.castShadow = true;
-   shape.receiveShadow = true;
-   
-   shape.position.set(
-    -458133.9720997899,
-    261,
-    5548777.776496544
-   );
-   
-   shape.rotation.set(
-    Math.random() * Math.PI,
-    Math.random() * Math.PI,
-    Math.random() * Math.PI
-   );
-   
-   
-   scene.add( shape );
-   
-   
-   new TWEEN.Tween(shape.material).to({opacity: 1}, 500).start();
-   
-  };
- 
- window.addEventListener('load',initScene);
- 
- 
- 
- 
- 
- 
- 
- 
- 
- /*
- window.addEventListener('load',function(){
-  var f = function(x){
-   return x;
-  };
-  var m = function(x,y,z){
-   if(x<y){
-    return y;
-   }else if(x>z){
-    return z;
-   }else{
-    return x;
-   }
-  };
-  var lastX = NaN,
-      lastY = NaN,
-      pressed = false;
-  
-  renderer.domElement.addEventListener('dragstart',function(e){e.preventDefault();});
-  renderer.domElement.addEventListener('mousedown',function(e){
-   pressed = e.buttons;
-   lastX = NaN;
-   lastY = NaN;
-   e.preventDefault();
-   return false;
-  });
-  renderer.domElement.addEventListener('mouseup',function(e){
-   pressed = false;
-   e.preventDefault();
-   return false;
-  });
-  renderer.domElement.addEventListener('mousemove',function(e){
-   if(!pressed){return;}
-   var deltaX, deltaY;
-   if(!isNaN(lastX+lastY)){
-    deltaX = e.clientX - lastX;
-    deltaY = e.clientY - lastY;
-    
-    if(pressed == 1){
-     camera.rotation.x = f(
-      camera.rotation.x,
-      camera.rotation.x = 0,
-      camera.translateZ( -deltaY/2 ),
-      camera.translateX( -deltaX/2 )
-     );
-    }else
-    if(pressed == 2){
-     camera.rotation.y -= deltaX/200;
-     camera.rotation.x += deltaY/200;
-     camera.rotation.x = m(camera.rotation.x, -Math.PI/2, Math.PI/2);
-    }
-    
-    
-   }
-   lastX = e.clientX;
-   lastY = e.clientY;
-  });
-  renderer.domElement.addEventListener('wheel',function(e){
-   var x = camera.rotation.x,
-       y = camera.rotation.y,
-       z = camera.rotation.z;
-   camera.rotation.set(0,0,0);
-   camera.translateY(e.deltaY);
-   camera.rotation.set(x,y,z);
-  });
-  renderer.domElement.addEventListener('contextmenu',function(e){
-   e.preventDefault();
-  });
- });
- */
+
+window.addEventListener('load',initScene);
--- browser.js
+++ browser.js
@@ -1,437 +1,53 @@
 window.browser = null;
-window.autopilot = null;
 
-var currentMapLocation = 0;
 
-//initialization
-function startDemo()
-{
-    //check map browser support (webgl)
-    if (Vadstena.checkSupport() == false) {
-        alert("Unfturtunately, 3D map browser need web browser support for WebGL. Sorry.");
+window.addEventListener('load',function(){
+    
+    //Check if browser supports WebGL
+    if( !Vadstena.checkSupport() ){
+        alert("This app needs WebGL, download a better browser.");
         return;
     }
-
-    var element_ = document.getElementById('map');
-
-    //init vadstena - 3D map library from melown.com
-    browser = new Vadstena.Browser('map', './source/mapConfig.json', './source/browserConfig.json', onBrowserInitialized, onBrowserUpdate);
-    browser.setCamera(60,5000);
-
-    //autopilot
-    autopilot = new Vadstena.Autopilot(browser, null);
-
-    //mouse events
-    document.onmousedown = onMouseDown;
-    document.oncontextmenu = (function(){ return false;});
-    document.onmouseup = onMouseUp;
-    document.onmousemove = onMouseMove;
-    window.addEventListener("DOMMouseScroll", onMouseWheel, true);
-    window.addEventListener("mousewheel", onMouseWheel, true);
-    document.onselectstart = function(){ return false; } //removes text cusor during draging
-
-    //keyboard events
-    document.onkeyup = onKeyUp;
-    document.onkeypress = onKeyPress;
-    document.onkeydown = onKeyDown;
-}
-
-//url params are not needed for this tutorial, so you can skip this function
-function getParamsFromURL() {
-    var queryString_ = {};
-    var query_ = window.location.search.substring(1);
-    var vars_ = query_.split("&");
-    for (var i=0; i < vars_.length; i++) {
-        var pair_ = vars_[i].split("=");
-        if (typeof queryString_[pair_[0]] === "undefined") {
-            queryString_[pair_[0]] = pair_[1];
-        } else if (typeof queryString_[pair_[0]] === "string") {
-            var arr_ = [ queryString_[pair_[0]], pair_[1] ];
-            queryString_[pair_[0]] = arr_;
-        } else {
-            queryString_[pair_[0]].push(pair_[1]);
-        }
-    }
-    return queryString_;
-}
-
-var autorotate = 0;
-var lastFrameTime = null;
-
-//once browser is initialized then this function is called
-function onBrowserInitialized(success_, message_) {
-    if (success_ == false) {
-        alert("Map browser initialization failed. Reason: " + message_);
-        return;
-    }
-
-    //set first location
-    flyToLocation(0, true);
-
-    //url params are not needed for this tutorial, you can skip this section
-    //url params - begin
-    var params = getParamsFromURL();
-
-    var pos = browser.getPosition();
-    var rot = browser.getOrientation();
-
-    if (params["x"] != null) { pos[0] = parseInt(params["x"], 10); }
-    if (params["y"] != null) { pos[1] = parseInt(params["y"], 10); }
-    if (params["d"] != null) { pos[2] = parseInt(params["d"], 10); }
-    if (params["rx"] != null) { rot[0] = parseInt(params["rx"], 10); }
-    if (params["ry"] != null) { rot[1] = parseInt(params["ry"], 10); }
-    if (params["rotate"] != null) { autorotate = parseFloat(params["rotate"]) * 0.001;}
-    if (params["controls"] == "0") {
-        document.getElementById("compassMain").style.display = "none";
-        document.getElementById("plus").style.display = "none";
-        document.getElementById("minus").style.display = "none";
-    }
-
-    browser.setPosition(pos[0], pos[1], pos[2]);
-    browser.setOrientation(rot[0], rot[1], rot[2]);
-    //url params - end
-
-    //setup comapss and zaoom buttons events
-    document.getElementById("compassMain").onmousedown = onBrowserCompass;
-    document.getElementById("plus").onclick = onBrowserZoomIn;
-    document.getElementById("minus").onclick = onBrowserZoomOut;
-
-    document.getElementById("compass").ondragstart = (function() { return false; });
-    document.getElementById("compass2").ondragstart = (function() { return false; });
-    document.getElementById("compass3").ondragstart = (function() { return false; });
-    document.getElementById("compassframe").ondragstart = (function() { return false; });
-    document.getElementById("compassframe2").ondragstart = (function() { return false; });
-    document.getElementById("compassframe3").ondragstart = (function() { return false; });
-    document.getElementById("plus").ondragstart = (function() { return false; });
-    document.getElementById("minus").ondragstart = (function() { return false; });
-
-    //hide stats panel
-    document.getElementById("renderStats").style.display = "none";
-
-    //link and info panel will ignore mouse dragging
-    document.getElementById("linkPanel").onmousedown = (function() { ignoreMove = true; });
-    document.getElementById("infoPanel").onmousedown = (function() { ignoreMove = true; });
-
-    //store time for autorotation
-    var time = (new Date()).getTime();
-    lastFrameTime = time;
-}
-
-//called when map is updated
-function onBrowserUpdate() {
-
-    //update compass orientation
-    var compass = document.getElementById("compass");
-    var compass2 = document.getElementById("compass2");
-    var compass3 = document.getElementById("compass3");
-    var orientation = browser.getOrientation();
-
-    var value = "rotateX("+((orientation[1]+90)*0.7)+"deg) " + "rotateZ("+(orientation[0]-45)+"deg)";
-
-    if (compass != null && compass2 != null && compass3 != null) {
-        compass.style.transform = value;
-        compass.style.webkitTransform = value;
-        compass.style.msTransform = value;
-
-        compass2.style.transform = value;
-        compass2.style.webkitTransform = value;
-        compass2.style.msTransform = value;
-
-        compass3.style.transform = value;
-        compass3.style.webkitTransform = value;
-        compass3.style.msTransform = value;
-    }
-
-    //update autorotation
-    var time = (new Date()).getTime();
-
-    if (autorotate != 0 && browser != null) {
-
-        var o = browser.getOrientation();
-
-        o[0] = (o[0] + (time - lastFrameTime) * autorotate) % 360;
-
-        browser.setOrientation(o[0], o[1], o[2]);
-
-        //call again
-        window.setTimeout(onBrowserUpdate, 1000 / 60);
-    }
-
-    lastFrameTime = time;
-}
-
-var compassMove = false;
-var compassFirst = false;
-var compassAngle = 0;
-var ignoreMove = false;
-
-//called when compass is dragged
-function onBrowserCompass() {
-    compassMove = true;
-    compassFirst = false;
-}
-
-//called when zoom in button is pressed
-function onBrowserZoomIn() {
-
-    if (browser == null){
-        return;
-    }
-
-    setTimeout(function(){ browser.zoom(-5); setTimeout(function(){ browser.zoom(-10); setTimeout(function(){ browser.zoom(-10); setTimeout(function(){ browser.zoom(-10); setTimeout(function(){ browser.zoom(-10); setTimeout(function(){ browser.zoom(-10); },60); },60);},60); },60); },60); },60);
-
-}
-
-//called when zoom out button is pressed
-function onBrowserZoomOut() {
-
-    if (browser == null){
-        return;
-    }
-
-    setTimeout(function(){ browser.zoom(5); setTimeout(function(){ browser.zoom(10); setTimeout(function(){ browser.zoom(10); setTimeout(function(){ browser.zoom(10); setTimeout(function(){ browser.zoom(10); setTimeout(function(){ browser.zoom(10); },60); },60);},60); },60); },60); },60);
-}
-
-//called when Shift+L is pressed
-function showLinkPanel() {
-
-    if (browser == null){
-        return;
-    }
-
-    //display link panel
-    document.getElementById("linkPanel").style.display = "block";
-
-    //fill in params
-    var pos = browser.getPosition();
-    var rot = browser.getOrientation();
-    var link_ = "";
-    link_ += "x=" + Math.round(pos[0]) + "&";
-    link_ += "y=" + Math.round(pos[1]) + "&";
-    link_ += "d=" + Math.round(pos[2]) + "&";
-    link_ += "rx=" + Math.round(rot[0]) + "&";
-    link_ += "ry=" + Math.round(rot[1]);
-
-    document.getElementById("linkPanelText").innerHTML = link_;
-}
-
-function hideLinkPanel() {
-    //hide link panel
-    document.getElementById("linkPanel").style.display = "none";
-}
-
-//called when Shift+S is pressed
-function switchStatsPanel() {
-    if(document.getElementById("renderStats").style.display != "none") {
-        //hide stats panel
-        document.getElementById("renderStats").style.display = "none";
-    } else {
-        //show stats panel
-        document.getElementById("renderStats").style.display = "block";
-    }
-}
-
-//keyboard events
-var altDown = false;
-var ctrlDown = false;
-var shiftDown = false;
-
-//mouse events
-var mouseRightDown = false;
-var mouseLeftDown = false;
-
-var mouseLX = 0;
-var mouseLY = 0;
-var mouseX = 0;
-var mouseY = 0;
-var mouseDX = 0;
-var mouseDY = 0;
-
-//called when mouse button is pressed
-function onMouseDown(event)
-{
-    var right = false;
-    var e = event || window.event;
-
-    if (e.which) { // Gecko (Firefox), WebKit (Safari/Chrome) & Opera
-        right = e.which == 3;
-    } else if (e.button) { // IE, Opera
-        right = e.button == 2;
-    }
-
-    if (right == true) {
-        mouseRightDown = true;
-    } else {
-        mouseLeftDown = true;
-    }
-
-    altDown = e.altKey;
-    ctrlDown = e.ctrlKey;
-    shiftDown = e.shiftKey;
-}
-
-//called when mouse button is released
-function onMouseUp(event)
-{
-    var right = false;
-    var e = event || window.event;
-
-    if (e.which) { // Gecko (Firefox), WebKit (Safari/Chrome) & Opera
-        right = e.which == 3;
-    } else if (e.button) { // IE, Opera
-        right = e.button == 2;
-    }
-
-    if (right == true) {
-        mouseRightDown = false;
-    } else {
-        mouseLeftDown = false;
-    }
-
-    compassMove = false;
-    ignoreMove = false;
-}
-
-//called when mouse moves
-function onMouseMove(event)
-{
-    var newX = event.clientX;
-    var newY = event.clientY;
-
-    mouseDX = (newX - mouseX);
-    mouseDY = (newY - mouseY);
-
-    mouseLX = mouseX;
-    mouseLY = mouseY;
-    mouseX = newX;
-    mouseY = newY;
-
-    var panFactor = 0.5;
-    var rotateFactor = 0.4;
-
-    if (ignoreMove == true) {
-        return;
-    }
-
-    if (compassMove == true) {
-
-        browser.rotate(mouseDX*rotateFactor*2, mouseDY*rotateFactor*2);
-        autorotate = 0;
-
-    } else {
-
-        // handle position change
-        if (mouseLeftDown == true && !(altDown == true || ctrlDown == true || shiftDown == true)) {
-            browser.pan(mouseDX*panFactor, mouseDY*panFactor);
-            autorotate = 0;
-        }
-
-        // handle orientation change
-        if (mouseRightDown == true || (mouseLeftDown == true && (altDown == true || ctrlDown == true || shiftDown == true))) {
-            browser.rotate(mouseDX*rotateFactor, mouseDY*rotateFactor);
-            autorotate = 0;
-        }
-
-    }
-}
-
-//called when mouse wheel is scrolled
-function onMouseWheel(event)
-{
-    if (event.preventDefault) {
-        event.preventDefault();
-    }
-
-    event.returnValue = false;
-
-    var delta = 0;
-    var w = event.wheelDelta
-    var d = event.detail;
-
-    if (d) {
-        if (w) delta = w/d/40*(d>=0?1:-1); // Opera
-        else delta = -d/3;                 // Firefox;         TO_DO: do not /3 for OS X
-    } else {
-        delta = w/120;                     // IE/Safari/Chrome TO_DO: /3 for Chrome OS X
-    }
-
-    if (isNaN(delta) == true) {
-        delta = 0;
-    }
-
-    var zoomFactor = 0.35;
-
-    //change camera distance
-    delta = (delta<0)?1:-1;
-
-    if (browser.getControlMode() == "observer") {
-        browser.zoom(delta*50*zoomFactor);
-    } else {
-        browser.zoom(delta*20*zoomFactor);
-    }
-}
-
-//keyboard events
-//called when keyboar key is pressed
-function onKeyDown(event)
-{
-    if (typeof event == 'undefined') {
-        event = window.event;
-    }
-
-    altDown = event.altKey;
-    ctrlDown = event.ctrlKey;
-    shiftDown = event.shiftKey;
-
-    onKeyUp(event, true);
-}
-
-function onKeyPress(event)
-{
-    onKeyUp(event, true);
-}
-
-//called when keyboar key is released
-function onKeyUp(event, press_)
-{
-    if (typeof event == 'undefined') {
-        event = window.event;
-    }
-
-    if (event) {
-        var keyCode_;
-
-        if (window.event) {         // eg. IE
-            keyCode_ = window.event.keyCode;
-        } else if (event.which) {   // eg. Firefox
-            keyCode_ = event.which;
-        } else {
-            keyCode_ = event.charCode;
-        }
-
-        /*
-        switch(keyCode_) {
-            //...
-        }
-        */
-
-        if (shiftDown == true && press_ != true) {
-            switch(keyCode_) {
-                case 76:
-                case 108:
-                    showLinkPanel();  break;  //key L pressed
-
-                case 83:
-                case 115:
-                    switchStatsPanel(); break; //key S pressed
-            }
-        }
-
-        //console.log("key" + keyCode);
-    }
-
-    altDown = event.altKey;
-    ctrlDown = event.ctrlKey;
-    shiftDown = event.shiftKey;
-}
+    
+    
+    //Init Vadstena
+    browser = new Vadstena.Browser(
+        
+        //Canvas element's id
+        'map',
+        
+        //Config files
+        './source/mapConfig.json',
+        './source/browserConfig.json',
+        
+        //Event listeners
+        onBrowserInitialized,
+        onBrowserUpdate
+        
+    );
+    
+    
+    
+    
+    //Called after Vadstena's init
+    function onBrowserInitialized(success_, message_){
+        
+        browser.setCamera(60,5000);
+        browser.setPosition(458133, 5548731, 261);
+        browser.setOrientation(-241, -37, 0);
+        
+    };
+    
+    
+    
+    //Called every frame
+    function onBrowserUpdate(){
+        
+        //empty for now
+        
+    };
+    
+    
+    
+});
 
