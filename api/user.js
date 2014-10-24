9e87b29d44761ac02345b4e12209ec8cbeeebd4b
DavidNovak <danov.dk.88@gmail.com>
2014-10-24 14:00:00 +0200

    Cam move
    Moving of camera with rat.

---
+++ api/user.js
@@ -0,0 +1,27 @@
+(function(){
+                    
+document.body.requestFullscreen =
+ document.body.requestFullscreen ||
+ document.body.msRequestFullscreen ||
+ document.body.mozRequestFullscreen ||
+ document.body.webkitRequestFullscreen;
+ 
+document.body.requestFullscreen();
+
+document.body.requestPointerLock = 
+ document.body.requestPointerLock ||
+ document.body.msRequestPointerLock ||
+ document.body.mozRequestPointerLock ||
+ document.body.webkitRequestPointerLock;
+ 
+document.body.requestPointerLock();
+
+document.addEventListener('mousemove',function(e){
+ camera.rotation.set(
+  camera.rotation.x + e.movementY,
+  camera.rotation.y + e.movementX,
+  camera.rotation.z 
+ );
+})
+
+})();
