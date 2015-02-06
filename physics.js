f0b687cfb2e2db8fa4db554d54878b0636da17ea
m93a <m93a.cz@gmail.com>
2015-02-06 16:06:35 +0100

    getting ready to rock
    +balloon mass, volume and temperature
    +world pressure, temperature

--- physics.js
+++ physics.js
@@ -8,14 +8,48 @@
   
   
   render = function(now){
-    var delta = now - then; //compute delta time
+    var delta = (now - then)/1000; //compute delta time
+    
+    var pressure = 101325;
+    var temp = 25 +273.15;
     
     //http://en.wikipedia.org/wiki/Hot_air_balloon#Generating_lift
     if(balloon){
+      
       if(!balloon.temp){
-        balloon.temp
+        balloon.temp = 110 +273.15;
       }
+      
+      if(!balloon.mass){
+        balloon.mass = 150;
+      }
+      
+      if(!balloon.volume){
+        balloon.volume = 600;
+      }
+      
+      if(!balloon.vy){
+        balloon.vy = 0;
+      }
+      
+      balloon.ay = 9.81*(
+        balloon.volume * ( pressure * (balloon.temp-temp) )
+        /( 287.058 * temp * balloon.temp * balloon.mass )
+      -1);
+      
+      balloon.vy += balloon.ay*delta;
+      if(balloon.vy > 100){
+        balloon.vy = 100;
+      }
+      if(balloon.vy < -100){
+        balloon.vy = -100;
+      }
+      
+      balloon.position.y += balloon.vy*delta;
+      
     }
+    
+    
     
     
     then = now; //save last known timestamp
